735450896bbeb9bdbd19d4828b71a9c13dd42304
Kylie Benfield <kylie@benfield.design>
2022-08-23 10:39:34 -0600

    Starting initial core engine structure

---
+++ Argo Core/Core.cs
@@ -0,0 +1,22 @@
+namespace Argo_Core;
+
+public class Core
+{
+    // Get System Modules
+        // Base Error Handlers
+        // File System Utilities
+            // Loggers
+            // Serializers
+            // Asset Loaders
+            // Save / Load
+            // Settings Handler
+        // OS Specific Modules
+        // Graphic Utilities
+        // Audio Utilities
+        // Input Utilities
+        // Networking Utilities
+        
+    // Get Registered Extensions 
+    // Handle Preloads
+    // Core Event Loop
+}
