9979cf8d6329cf136e7a0878e3bfd4689d35fadb
Kylie Benfield <kylie@benfield.design>
2022-08-22 23:20:45 -0600

    Adjusting new class format to not need type if it is implicit

--- Argo Core/Class1.cs
+++ Argo Core/Class1.cs
@@ -2,7 +2,7 @@
 
 public class Class1
 {
-    public bool testCase()
+    public static bool TestCase()
     {
         return true;
     }
--- Argo Tests/UnitTest1.cs
+++ Argo Tests/UnitTest1.cs
@@ -8,8 +8,7 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-        Class1 cls = new Class1();
-        
-        Assert.True(cls.testCase());
+        Class1 cls = new();
+        Assert.True(Class1.TestCase());
     }
 }
